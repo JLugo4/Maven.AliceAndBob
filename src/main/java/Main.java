eb610af332202cdb99904efdd4c1d485caef395a
Jarek <jarek@Zipcoders-MacBook-Pro-65.local>
2023-03-02 20:55:14 -0500

    I have greeted Alice or Bob

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,16 @@
+import java.util.Scanner;
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter your name(s)");
+        String name1 = scanner.nextLine();
+        if (name1.equals("Alice") || (name1.equals("Bob"))) {
+            System.out.println("Hello " + name1 + " how are you?");
+        }
 
     }
 }
